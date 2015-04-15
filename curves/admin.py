52df0e836e1ccb17fa63ffd0f958539617e94a39
Suraj Bhat <smbhat@nat-oitwireless-inside-vapornet100-a-25031.Princeton.EDU>
2015-04-14 21:41:54 -0400

    fixed admin file

--- curves/admin.py
+++ curves/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from curves.models import Course_Specific, User
+
+class Course_Specific_Admin(admin.ModelAdmin):
+	search_fields = ['semester', 'prof', 'dept', 'num', 'name']
+
+class User_Admin(admin.ModelAdmin):
+	search_fields = ['netid', 'name', 'year']
+
+admin.site.register(Course_Specific, Course_Specific_Admin)
+admin.site.register(User, User_Admin)
